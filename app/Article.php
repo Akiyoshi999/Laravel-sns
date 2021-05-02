85f8e1dc6f274a18aa757d528640cfbff4c992a0
Akiyoshi <zundokokiyoshi999@gmail.com>
2021-05-03 08:27:35 +0900

    ユーザーがいいね済みかの判定

--- app/Article.php
+++ app/Article.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Article extends Model
 {
@@ -16,5 +17,19 @@ class Article extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo('App\user');
+    }
+
+    // likeテーブルへの紐付け
+    public function likes(): BelongsToMany
+    {
+        return $this->belongsToMany('App\User', 'likes')->withTimestamps();
+    }
+
+    // ユーザーがいいね済みかの判定
+    public function isLikeBy(?User $user): bool
+    {
+        return $user
+            ? (bool)$this->likes->where('id', $user->id)->count()
+            : false;
     }
 }
