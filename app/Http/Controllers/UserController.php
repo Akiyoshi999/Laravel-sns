f6cd3e97133b05a21fb421f0793f2e00bab4268f
Akiyoshi <zundokokiyoshi999@gmail.com>
2021-05-05 08:54:27 +0900

    N+1問題改善

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,13 @@ class UserController extends Controller
 {
     public function show(string $name)
     {
-        $user = User::where('name', $name)->first();
+        $user = User::where('name', $name)->first()
+            ->load([
+                'articles.user',
+                'articles.likes',
+                'articles.tags',
+            ]);
+
         $articles = $user->articles->sortByDesc('created_at');
 
         return view('users.show', [
@@ -22,7 +28,12 @@ class UserController extends Controller
     // いいねした記事の返却
     public function likes(string $name)
     {
-        $user = User::where('name', $name)->first();
+        $user = User::where('name', $name)->first()
+            ->load([
+                'likes.user',
+                'likes.likes',
+                'likes.tags',
+            ]);
         $articles = $user->likes->sortByDesc('created_at');
 
         return view('users.likes', [
@@ -34,7 +45,10 @@ class UserController extends Controller
     // フォローユーザー表示
     public function followings(string $name)
     {
-        $user = User::where('name', $name)->first();
+        $user = User::where('name', $name)->first()
+            ->load([
+                'followings.followers'
+            ]);
         $followings = $user->followings->sortByDesc('created_at');
 
         return view('users.followings', [
@@ -46,7 +60,10 @@ class UserController extends Controller
     // フォロワーユーザー表示
     public function followers(string $name)
     {
-        $user = User::where('name', $name)->first();
+        $user = User::where('name', $name)->first()
+            ->load([
+                'followers.followers'
+            ]);
         $followers = $user->followers->sortByDesc('created_at');
 
         return view('users.followers', [
