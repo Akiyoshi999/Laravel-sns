31799f1874de11a838ed17aa5a6a850339815e15
Akiyoshi <zundokokiyoshi999@gmail.com>
2021-05-04 16:49:36 +0900

    Googleアカウントでログイン可能

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 Route::prefix('login')->name('login.')->group(function () {
     Route::get('/{provider}', 'Auth\LoginController@redirectToProvider')->name('{provider}');
+    Route::get('/{provider}/callback', 'Auth\LoginController@handleProviderCallback')->name('{provider}.callback');
 });
 Route::get('/', 'ArticleController@index')->name('articles.index');
 Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
