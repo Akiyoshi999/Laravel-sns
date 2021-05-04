edb26072482e8c2ea3fdd5ead476dcd00174ed65
Akiyoshi <zundokokiyoshi999@gmail.com>
2021-05-04 09:14:07 +0900

    ユーザーページに記事一覧を表示する処理追加

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -34,5 +34,19 @@
         </div>
       </div>
     </div>
+    <ul class="nav nav-tabs nav-justified mt-3">
+      <li class="nav-item"><a class="nav-link text-muted activ"
+          href="{{ route('users.show', ['name' => $user->name]) }}">
+          記事
+        </a>
+      </li>
+      <li class="nav-item"><a href="" class="nav-link text-muted">
+          いいね
+        </a>
+      </li>
+    </ul>
+    @foreach ($articles as $article)
+      @include('articles.card')
+    @endforeach
   </div>
 @endsection
